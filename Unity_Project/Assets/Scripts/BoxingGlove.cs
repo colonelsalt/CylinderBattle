3a80407badc852c0fffeaa431ecfc3f388098f95
Sondre Agledahl <sondre.milch@gmail.com>
2017-12-25 23:05:06 +0100

    Punching animation now looks pretty nice (will need a tiny bit more tweaking later)

--- Unity_Project/Assets/Scripts/BoxingGlove.cs
+++ Unity_Project/Assets/Scripts/BoxingGlove.cs
@@ -10,6 +10,8 @@ public class BoxingGlove : MonoBehaviour
     // How much damage being struck by this glove deals
     [SerializeField] private int m_Damage;
 
+    [SerializeField] private float m_TimeBetweenPunches;
+
     // --------------------------------------------------------------
 
     // Reference to PlayerController this is attached to
@@ -18,6 +20,9 @@ public class BoxingGlove : MonoBehaviour
     private Animator m_Animator;
 
     private int m_NumPunches = 0;
+
+    private bool m_PunchedThisFrame = false;
+
 
     // --------------------------------------------------------------
 
@@ -29,16 +34,31 @@ public class BoxingGlove : MonoBehaviour
     
     private void Update()
     {
-        if (InputHelper.FireButtonPressed(m_Player.PlayerNum))
+        if (InputHelper.FireButtonPressed(m_Player.PlayerNum) && !m_PunchedThisFrame)
         {
-            Punch();
+            m_PunchedThisFrame = true;
+            InvokeRepeating("Punch", 0.00001f, m_TimeBetweenPunches);
+        }
+        if (InputHelper.FireButtonReleased(m_Player.PlayerNum))
+        {
+            m_PunchedThisFrame = false;
+            CancelInvoke("Punch");
         }
     }
 
     // Trigger Animator to thrust gloves forward
     private void Punch()
     {
+        m_NumPunches++;
         m_Animator.SetTrigger("PunchTrigger");
+        m_Animator.SetInteger("NumPunches", m_NumPunches);
+    }
+
+    // Called from the animator when final punch animation in sequence finishes
+    private void ResetNumPunches()
+    {
+        m_NumPunches = 0;
+        m_Animator.SetInteger("NumPunches", m_NumPunches);
     }
 
     // When either glove strikes something, damage it
